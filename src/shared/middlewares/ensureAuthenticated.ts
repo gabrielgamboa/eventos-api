03d297bde8d98bbfec0384347bcadc873947249b
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-10 12:17:56 -0300

    criando middleware para autenticação

---
+++ src/shared/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../../database/repositories/implementations/UsersRepository";
+import { AppError } from "../errors/AppError";
+
+
+interface ITokenPayload {
+    id: number;
+    iat: number;
+    exp: number;
+}
+
+export async function ensureAuthenticated (request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("Token missing", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        //pego por desestruturação o sub, porém nomeando-o de "user_id".
+        const { id: user_id } = verify(token, "d5c6d7e0392032715f66cbd14e085594") as ITokenPayload;
+
+        const usersRepository = new UsersRepository();
+        const user = usersRepository.findById(user_id);
+
+        if(!user) {
+            throw new AppError("User does not exists", 401);
+        }
+
+        request.user = user;
+
+        return next();
+
+    } catch (error) {
+        throw new AppError("Invalid token", 401);
+    }
+
+}
