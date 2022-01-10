d7634c010f2b6fba1df47a38bf59599d70461df4
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-10 11:19:02 -0300

    finalizando rota de autenticação de usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserService.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../../../database/repositories/IUsersRepository";
+import { AppError } from "../../../../shared/errors/AppError";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+export class AuthenticateUserService {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) { }
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user)
+            throw new AppError("E-mail or password is incorrect!", 401);
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch)
+            throw new AppError("E-mail or password is incorrect!", 401);
+
+            const token = sign({ id: user.id }, "d5c6d7e0392032715f66cbd14e085594", {
+                expiresIn: "1d"
+            });
+    
+            const tokenReturn: IResponse = {
+                user: {
+                    name: user.name,
+                    email: user.email
+                },
+                token,
+            }
+    
+            return tokenReturn;
+    }
+}
