976f42b9196ceaa6a9bc0ea334f60b0ea71c9c60
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-15 01:56:39 -0300

    iniciando middleware para validar campos

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { CreateUserService } from "./CreateUserService";
+import { createUserSchema } from "../../../../shared/validators";
 
 export class CreateUserController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, email, password, cpf, phone } = req.body;
+
+        const result = createUserSchema.validate(req.body, {
+            abortEarly: false
+        });
 
         const createUserService = container.resolve(CreateUserService);
         const createdUser = await createUserService.execute({ name, email, password, cpf, phone });
---
+++ src/shared/middlewares/createUserMiddleware.ts
@@ -0,0 +1,5 @@
+// import { NextFunction, Request, Response  } from "express";
+
+// export async function createUserMiddleware(req: Request, res: Response, next: NextFunction): Promise<Response> {
+
+// }
