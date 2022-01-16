007082edaa65666126944315ea6b50026d372fa6
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-16 20:35:47 -0300

    alterando schema de criação de usuário

--- src/shared/validators/createUserSchema.validator.ts
+++ src/shared/validators/createUserSchema.validator.ts
@@ -4,6 +4,6 @@ export const createUserSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    cpf: Joi.string().max(11).required(),
-    phone: Joi.string().max(11)
+    cpf: Joi.string().min(11).required(),
+    phone: Joi.string().min(11)
 });
